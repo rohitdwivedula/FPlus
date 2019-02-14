9c36b4980f5fbc4403852b7d4ebcf47372be2bae
rohitdwivedula <rohitdwivedula@gmail.com>
2019-02-14 15:12:29 +0530

    Initial Commit
    Add SQL queries/commands to create empty database required for this project.

---
+++ Setup/db_setup.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE fplus;
+USE fplus;
+
+CREATE TABLE users (
+	username varchar(25) NOT NULL,
+	firstname varchar(255) NOT NULL,
+	lastname varchar(255) NOT NULL,
+	email varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
+	salt varchar(255) NOT NULL,
+	phone varchar(20),
+	dob date,
+	account_created_on datetime NOT NULL,
+	sex varchar(255),
+	PRIMARY KEY (username)
+);
+
+CREATE TABLE posts (
+	post_id int NOT NULL AUTO_INCREMENT,
+	username varchar(25) NOT NULL,
+	body TEXT NOT NULL,
+	media TEXT,
+	visibility int NOT NULL,
+	posted_on datetime NOT NULL,
+	PRIMARY KEY (post_id),
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE comments (
+	comment_id int NOT NULL AUTO_INCREMENT,
+	post_id int NOT NULL,
+	username varchar(25) NOT NULL,
+	comment_text TEXT NOT NULL,
+	commented_on datetime NOT NULL,
+	parent int,
+	PRIMARY KEY (comment_id),
+	FOREIGN KEY (post_id) REFERENCES posts(post_id),
+	FOREIGN KEY (username) REFERENCES users(username),
+	FOREIGN KEY (parent) REFERENCES comments(comment_id)
+);
+
+CREATE TABLE reactions (
+	react_id int NOT NULL AUTO_INCREMENT,
+	post_id int NOT NULL,
+	username varchar(25) NOT NULL,
+	react_type int NOT NULL,
+	PRIMARY KEY (react_id),
+	FOREIGN KEY (post_id) REFERENCES posts(post_id),
+	FOREIGN KEY (username) REFERENCES users(username)
+);
